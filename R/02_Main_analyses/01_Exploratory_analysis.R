0d24783bdc92d1adb92f35010276c337139e38b9
Natálie Námešná 🌱✨🌍 <hanusova.nat@gmail.com>
2026-07-08 16:29:06 +0200

    explonatory analysis for year

--- R/02_Main_analyses/01_Exploratory_analysis.R
+++ R/02_Main_analyses/01_Exploratory_analysis.R
@@ -79,16 +79,43 @@ ggplot(
 # 4. Year   -----
 #----------------------------------------------------------#
 case_studies_year <- case_studies |> 
-  select(id, year)
+  select(id, year) |> 
+  count(year, name= "n", sort = TRUE)
 
-ggplot(case_studies_year, aes(x = year)) +
-  geom_bar()
+ggplot(
+  data = case_studies_year,
+  mapping = aes(
+    y=  reorder(year, n),
+    x= n,
+  )
+) +
+  xlim(0,7) +
+  geom_col() +
+  labs(
+    title = "Number of case studies published in particular years",
+    x = "n",
+    y = "Year",
+  )+
+  scale_x_continuous(breaks = seq(0, 7, by = 1)) +
+  coord_cartesian(expand = FALSE) +
+  theme_minimal(base_size = 15) +
+  theme(
+    legend.position = "none",
+    legend.title = element_blank(),
+    plot.title = element_text(
+      face = "bold",
+      margin = margin(b = 10)
+    ),
+    plot.title.position = "plot",
+    plot.margin = margin(15, 10, 10, 15)
+  )
 
 
 #----------------------------------------------------------#
 # 5. Region   -----
 #----------------------------------------------------------#
-
+case_studies_region <- case_studies |> 
+  select(id, region)
 
 #----------------------------------------------------------#
 # 6. Number of pollen data   -----
