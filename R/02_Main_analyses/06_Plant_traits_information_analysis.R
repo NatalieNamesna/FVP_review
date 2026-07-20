c0e65bba6c19c3ccdebbd43e9c744b7463c15f85
Natálie Námešná 🌱✨🌍 <hanusova.nat@gmail.com>
2026-07-20 14:03:05 +0200

    waffle plot for the leaf traits

--- R/02_Main_analyses/06_Plant_traits_information_analysis.R
+++ R/02_Main_analyses/06_Plant_traits_information_analysis.R
@@ -24,18 +24,30 @@ install.packages("ggtext")
 install.packages("showtext")
 install.packages("sysfonts")
 install.packages("rcartocolor")
+install.packages("GGally")
+install.packages("glue")
+install.packages("marquee")
+install.packages("readr")
+install.packages("stringr")
+
+
 library(rcartocolor)
 library(sysfonts)
 library(showtext)
 library(ggtext)
 library(waffle)
-library(ggwaffle)
 library(tidyverse)
 library(ggplot2)
 library(ggtext)
 library(sf) 
 library(here)
 library(dplyr)
+library(GGally)
+library(glue)
+library(marquee)
+library(readr)
+library(stringr)
+
 
 # Load the table with case studies
 
@@ -235,7 +247,7 @@ waffle_chart_leaf_traits <- ggplot(data = waffle_leaf_traits) +
     ),
     flip = TRUE,
     n_rows = 10,
-    size = 2,
+    size = 1,
     family = "Font Awesome 7"
   ) +
   facet_wrap(~variable,
@@ -252,6 +264,7 @@ icons_plot_leaf_traits <- waffle_chart_leaf_traits +
     values = "leaf",
     guide = "none"
   )
+
 icons_plot_leaf_traits
 
 
@@ -264,11 +277,128 @@ display_carto_all(
 )
 
 
+# colors ----
+col_palette <- carto_pal(
+  n = length(unique(waffle_leaf_traits$value)) + 1,
+  name = "Vivid"
+)[1:length(unique(waffle_leaf_traits$value))]
+
+# vector of T and F
+true_false <- unique(waffle_leaf_traits$value)
+
+# T and F now have their own colors
+names(col_palette) <- true_false
+
+# plot with new colors and icons
+col_plot_leaf_traits <- icons_plot_leaf_traits +
+  scale_color_manual(
+    values = col_palette,
+    guide = "none"
+  )
+
+# Adding style text ----
+
+## title and caption ----
+title <- "**Which leaf traits were used in the case studies?**"
+cap <- "**Data**: Analyses of 62 case studies | **Graphic**: N. Namesna, inspired by N. Rennie "
 
 
+source_caption <- function(source, graphic, sep = " | ") {
+  caption <- glue::glue(
+    "**Data**: {source}{sep}**Graphic**: {graphic}"
+  )
+  return(caption)
+}
+
+cap <- source_caption(
+  source = "62 case studies",
+  graphic = "N. Namesna, inspired by N. Rennie"
+)
+cap
+
+## subtitle ----
+st <- marquee_glue(
+  "The analysis of 62 case studies. It shows how many case studies used a particular leaf trait - 
+   {.{col_palette[[2]]} {names(col_palette)[[2]]}}, or not - 
+  {.{col_palette[[1]]} {names(col_palette)[[1]]}} in their analyses."
+)
+
+# text plot ----
+text_plot_leaf_traits <- col_plot_leaf_traits +
+  labs(
+    title = title,
+    subtitle = st,
+    caption = cap
+  )
+text_plot_leaf_traits
 
 
+# scale plot ----
+scale_plot_leaf_traits <- text_plot_leaf_traits +
+  scale_y_continuous(
+    expand = c(0, 0),
+    breaks = c(1, 2, 3, 4, 5, 6, 7),              # Every 2 rows
+    labels = c("10","20","30", "40", "50", "60", "70"), # Converts rows back to "counts" if desired
+    limits = c(0, 10)                        # Caps it perfectly at your n_rows height
+  ) +
+  coord_fixed()
 
+
+# final touches ----
+scale_plot_leaf_traits +
+  theme_minimal(
+    base_size = 9
+  )  +
+  theme(
+    # spacing around text and plot
+    plot.title.position = "plot",
+    plot.caption.position = "plot",
+    plot.margin = margin(10, 20, 10, 20),
+    # background and grid lines
+    plot.background = element_rect(
+      fill = bg_col, color = bg_col
+    ),
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_line(
+      linewidth = 0.4
+    ),
+    panel.grid.minor = element_blank(),
+    axis.text.x = element_blank(),
+    # format text with marquee
+    plot.title = element_marquee(
+      color = text_col,
+      width = 1,
+      size = 30
+    ),
+    plot.subtitle = element_marquee(
+      color = text_col,
+      width = 1,
+      size = 20
+    ),
+    plot.caption = element_marquee(
+      hjust = 0,
+      lineheight = 0.5,
+      size = 15,
+      margin = margin(t = 5)
+    ),
+      strip.text = element_marquee(
+        size = 15,
+       # face = "bold"
+      ),
+    panel.spacing.x = unit(0.15, "lines"),
+    axis.text.y = element_marquee(size = 4)
+   
+    )
+
+
+# save it ----
+ggplot2::ggsave(
+  plot = scale_plot_leaf_traits,
+  filename = here::here("Outputs/Figures/scale_plot_leaf_traits.png"),
+  width = 10,       # Explicitly set width in inches
+  height = 8,       # Explicitly set height in inches
+  dpi = 300         # Matches your showtext resolution!
+)
 
 
 
