3b03d7cfdc9ef38a5f5051ed6b1a2f7623691f69
Natálie Námešná 🌱✨🌍 <hanusova.nat@gmail.com>
2026-07-23 12:04:24 +0200

    code for combined waffle plot for pollen info

--- R/02_Main_analyses/05_Pollen_information_analysis.R
+++ R/02_Main_analyses/05_Pollen_information_analysis.R
@@ -88,7 +88,7 @@ case_studies_pollen_waffle <- case_studies_pollen |>
     counts_correction = as.character(pollen_counts_corrections),
     age_depth_recalculation = as.character(age_depth_recalculation)
   ) |> 
-  select(c(12:21)) |> 
+  select(c(11:21)) |> 
   pivot_longer(
     everything(),
     names_to = "variable",
@@ -208,7 +208,7 @@ waffle_chart_pollen <- ggplot(data = case_studies_pollen_waffle) +
 waffle_chart_pollen
 
 
-# add icons to leaf traits waffle ----
+# add icons  ----
 icons_plot_pollen <- waffle_chart_pollen +
   scale_label_pictogram(
     values = "circle",
@@ -360,7 +360,500 @@ ggplot2::ggsave(
 
 # data ----
 case_studies_pollen_method_waffle <- case_studies_pollen_waffle |> 
-  slice(1:12)
+  slice(4:15)
+
+# font ----
+font_add(
+  family = "Font Awesome 7",
+  regular = "Data/Input/fonts/Font Awesome 7 Free-Solid-900.otf"
+)
+showtext_auto()
+showtext_opts(dpi = 300)
+
+
+# basic plot ----
+waffle_chart_pollen_method <- ggplot(data = case_studies_pollen_method_waffle) +
+  geom_pictogram(
+    mapping = aes(
+      label = value,
+      color = value,
+      values = count
+    ),
+    flip = TRUE,
+    n_rows = 10,
+    size = 1,
+    family = "Font Awesome 7"
+  ) +
+  facet_wrap(~variable,
+             nrow = 1,
+             strip.position = "bottom"
+  )
+
+waffle_chart_pollen_method
+
+
+# add icons ----
+icons_plot_pollen_method <- waffle_chart_pollen_method +
+  scale_label_pictogram(
+    values = "circle",
+    guide = "none"
+  )
+
+icons_plot_pollen_method
+
+# advanced styling ----
+bg_col <- "#FAFAFA"
+text_col <- "black"
+
+# colors ----
+col_palette_pollen_method <-  c("#E84746",  "#BFC2C1", "#509B51")
+
+# vector of T and F
+true_false_NA_pollen_method <- unique(case_studies_pollen_method_waffle$value)
+
+# T and F now have their own colors
+names(col_palette_pollen_method) <- true_false_NA_pollen_method
+
+# plot with new colors and icons
+col_plot_pollen_method <- icons_plot_pollen_method +
+  scale_color_manual(
+    values = col_palette_pollen_method,
+    guide = "none"
+  )
+
+# Adding style text ----
+
+## title and caption ----
+title_pollen_method <- "Method of choosing pollen records"
+
+# text plot ----
+text_plot_pollen_method <- col_plot_pollen_method +
+  labs(
+    title = title_pollen_method
+  )
+text_plot_pollen_method
+
+
+# scale plot ----
+scale_plot_pollen_method <- text_plot_pollen_method +
+  scale_y_continuous(
+    expand = c(0, 0),
+    breaks = c(1, 2, 3, 4, 5, 6, 7),              # Every 2 rows
+    labels = c("10","20","30", "40", "50", "60", "70"), # Converts rows back to "counts" if desired
+    limits = c(0, 10)                        # Caps it perfectly at your n_rows height
+  ) +
+  coord_fixed()
+
+
+# final touches ----
+waffle_plot_pollen_method <- scale_plot_pollen_method +
+  theme_minimal(
+    base_size = 9
+  )  +
+  theme(
+    # spacing around text and plot
+    plot.title.position = "panel",
+    #  plot.caption.position = "plot",
+    plot.margin = margin(0, 0, 0, 0),
+    # background and grid lines
+    plot.background = element_rect(
+      fill = bg_col, color = bg_col
+    ),
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_line(
+      linewidth = 0.3
+    ),
+    panel.grid.minor = element_blank(),
+    axis.text.x = element_blank(),
+    # axis.text.y = element_blank(),
+    # format text with marquee
+    plot.title = element_marquee(
+      color = text_col,
+      width = 1,
+      size = 15,
+      # face = "bold"
+    ),
+    strip.text = element_marquee(
+      size = 11,
+      # face = "bold"
+    ),
+    panel.spacing.x = unit(0.1, "lines"),
+    axis.text.y = element_marquee(size = 3)
+    
+  )  + facet_wrap(~variable, ncol = 1, strip.position = "bottom")
+
+
+waffle_plot_pollen_method
+
+
+
+#-----------------------------------------------------------------------------#
+
+# 5. waffle plot for data processing ----
+
+#-----------------------------------------------------------------------------#
+
+# data ----
+case_studies_pollen_process_waffle <- case_studies_pollen_waffle |> 
+  slice(1,2,3,16,17,18,25,26,27)
+
+
+# font ----
+font_add(
+  family = "Font Awesome 7",
+  regular = "Data/Input/fonts/Font Awesome 7 Free-Solid-900.otf"
+)
+showtext_auto()
+showtext_opts(dpi = 300)
+
+
+# basic plot ----
+waffle_chart_pollen_process <- ggplot(data = case_studies_pollen_process_waffle) +
+  geom_pictogram(
+    mapping = aes(
+      label = value,
+      color = value,
+      values = count
+    ),
+    flip = TRUE,
+    n_rows = 10,
+    size = 1,
+    family = "Font Awesome 7"
+  ) +
+  facet_wrap(~variable,
+             nrow = 1,
+             strip.position = "bottom"
+  )
+
+waffle_chart_pollen_process
+
+
+# add icons ----
+icons_plot_pollen_process <- waffle_chart_pollen_process +
+  scale_label_pictogram(
+    values = "circle",
+    guide = "none"
+  )
+
+icons_plot_pollen_process
+
+# advanced styling ----
+bg_col <- "#FAFAFA"
+text_col <- "black"
+
+# colors ----
+col_palette_pollen_process <-  c("#E84746",  "#BFC2C1", "#509B51")
+
+# vector of T and F
+true_false_NA_pollen_process <- unique(case_studies_pollen_process_waffle$value)
+
+# T and F now have their own colors
+names(col_palette_pollen_process) <- true_false_NA_pollen_process
+
+# plot with new colors and icons
+col_plot_pollen_process <- icons_plot_pollen_process +
+  scale_color_manual(
+    values = col_palette_pollen_process,
+    guide = "none"
+  )
+
+# Adding style text ----
+
+## title and caption ----
+title_pollen_process <- "Data processing"
+
+# text plot ----
+text_plot_pollen_process <- col_plot_pollen_process +
+  labs(
+    title = title_pollen_process
+  )
+text_plot_pollen_process
+
+
+# scale plot ----
+scale_plot_pollen_process <- text_plot_pollen_process +
+  scale_y_continuous(
+    expand = c(0, 0),
+    breaks = c(1, 2, 3, 4, 5, 6, 7),              # Every 2 rows
+    labels = c("10","20","30", "40", "50", "60", "70"), # Converts rows back to "counts" if desired
+    limits = c(0, 10)                        # Caps it perfectly at your n_rows height
+  ) +
+  coord_fixed()
+
+
+# final touches ----
+waffle_plot_pollen_process <- scale_plot_pollen_process +
+  theme_minimal(
+    base_size = 9
+  )  +
+  theme(
+    # spacing around text and plot
+    plot.title.position = "panel",
+    #  plot.caption.position = "plot",
+    plot.margin = margin(0, 0, 0, 0),
+    # background and grid lines
+    plot.background = element_rect(
+      fill = bg_col, color = bg_col
+    ),
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_line(
+      linewidth = 0.3
+    ),
+    panel.grid.minor = element_blank(),
+    axis.text.x = element_blank(),
+    # axis.text.y = element_blank(),
+    # format text with marquee
+    plot.title = element_marquee(
+      color = text_col,
+      width = ,
+      size = 15,
+      # face = "bold"
+    ),
+    strip.text = element_marquee(
+      size = 11,
+      # face = "bold"
+    ),
+    panel.spacing.x = unit(0.1, "lines"),
+    axis.text.y = element_marquee(size = 3)
+    
+  )  + facet_wrap(~variable, ncol = 1, strip.position = "bottom")
+
+
+waffle_plot_pollen_process
+
+#-----------------------------------------------------------------------------#
+
+# 6. waffle plot for data source ----
+
+#-----------------------------------------------------------------------------#
+
+# data ----
+case_studies_pollen_source_waffle <- case_studies_pollen_waffle |> 
+  slice(19:24, 28:33)
+
+# font ----
+font_add(
+  family = "Font Awesome 7",
+  regular = "Data/Input/fonts/Font Awesome 7 Free-Solid-900.otf"
+)
+showtext_auto()
+showtext_opts(dpi = 300)
+
+
+# basic plot ----
+waffle_chart_pollen_source <- ggplot(data = case_studies_pollen_source_waffle) +
+  geom_pictogram(
+    mapping = aes(
+      label = value,
+      color = value,
+      values = count
+    ),
+    flip = TRUE,
+    n_rows = 10,
+    size = 1,
+    family = "Font Awesome 7"
+  ) +
+  facet_wrap(~variable,
+             nrow = 1,
+             strip.position = "bottom"
+  )
+
+waffle_chart_pollen_source
+
+
+# add icons ----
+icons_plot_pollen_source <- waffle_chart_pollen_source +
+  scale_label_pictogram(
+    values = "circle",
+    guide = "none"
+  )
+
+icons_plot_pollen_source
+
+# advanced styling ----
+bg_col <- "#FAFAFA"
+text_col <- "black"
+
+# colors ----
+col_palette_pollen_source <-  c("#E84746",  "#BFC2C1", "#509B51")
+
+# vector of T and F
+true_false_NA_pollen_source <- unique(case_studies_pollen_source_waffle$value)
+
+# T and F now have their own colors
+names(col_palette_pollen_source) <- true_false_NA_pollen_source
+
+# plot with new colors and icons
+col_plot_pollen_source <- icons_plot_pollen_source +
+  scale_color_manual(
+    values = col_palette_pollen_source,
+    guide = "none"
+  )
+
+# Adding style text ----
+
+## title and caption ----
+title_pollen_source <- "Source of pollen data"
+
+# text plot ----
+text_plot_pollen_source <- col_plot_pollen_source +
+  labs(
+    title = title_pollen_source
+  )
+text_plot_pollen_source
+
+
+# scale plot ----
+scale_plot_pollen_source <- text_plot_pollen_source +
+  scale_y_continuous(
+    expand = c(0, 0),
+    breaks = c(1, 2, 3, 4, 5, 6, 7),              # Every 2 rows
+    labels = c("10","20","30", "40", "50", "60", "70"), # Converts rows back to "counts" if desired
+    limits = c(0, 10)                        # Caps it perfectly at your n_rows height
+  ) +
+  coord_fixed()
+
+
+# final touches ----
+waffle_plot_pollen_source <- scale_plot_pollen_source +
+  theme_minimal(
+    base_size = 9
+  )  +
+  theme(
+    # spacing around text and plot
+    plot.title.position = "panel",
+    #  plot.caption.position = "plot",
+    plot.margin = margin(0, 0, 0, 0),
+    # background and grid lines
+    plot.background = element_rect(
+      fill = bg_col, color = bg_col
+    ),
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_line(
+      linewidth = 0.3
+    ),
+    panel.grid.minor = element_blank(),
+    axis.text.x = element_blank(),
+    # axis.text.y = element_blank(),
+    # format text with marquee
+    plot.title = element_marquee(
+      color = text_col,
+      width = 1,
+      size = 15,
+      # face = "bold"
+    ),
+    strip.text = element_marquee(
+      size = 11,
+      # face = "bold"
+    ),
+    panel.spacing.x = unit(0.1, "lines"),
+    axis.text.y = element_marquee(size = 3)
+    
+  )  + facet_wrap(~variable, ncol = 1, strip.position = "bottom")
+
+
+waffle_plot_pollen_source
+
+
+
+#-----------------------------------------------------------------------------#
+
+# 7. combined waffle plot for data source, processing, choosing method ----
+
+#-----------------------------------------------------------------------------#
+
+
+# we will combined these three plots -----
+waffle_plot_pollen_method
+waffle_plot_pollen_source
+waffle_plot_pollen_process
+
+
+# color palette ----
+col_palette_pollen_method
+col_palette_pollen_process
+col_palette_pollen_source
+
+# title and caption ----
+title_combined_plot_pollen <- "Information about pollen records obtained from case studies"
+
+
+# subtitle ----
+st_combined_plot_pollen <- marquee_glue(
+  "The analysis of 62 case studies.\n
+{.{col_palette_pollen_method[[3]]} {names(col_palette_pollen_method)[[3]]}}, {.{col_palette_pollen_method[[1]]} {names(col_palette_pollen_method)[[1]]}}, {.{col_palette_pollen_method[[2]]} {names(col_palette_pollen_method)[[2]]}} "
+)
+
+
+# combined plot ----
+combined_plot_pollen <-
+  waffle_plot_pollen_method +
+  waffle_plot_pollen_source + 
+  waffle_plot_pollen_process +
+  plot_layout(widths = c(4, 4, 3)) + # <--- Force widths based on number of categories!
+  plot_annotation(
+    title = title_combined_plot_pollen,
+    subtitle = st_combined_plot_pollen,
+    theme = theme(
+      plot.title = element_marquee(
+        size = 30,
+        width = 1,
+        hjust = 0
+      ),
+      plot.subtitle = element_marquee(
+        size = 15,
+        hjust = 0,
+        width = 1
+      )
+    )
+  ) 
+
+combined_plot_pollen
+
+
+
+# better sizing ----
+library(patchwork)
+
+# 1. Stack a 1-unit empty space underneath the 3-item Data Processing column
+process_column <- waffle_plot_pollen_process / plot_spacer() + 
+  plot_layout(heights = c(4, 1))
+
+# 2. Combine the 3 vertical columns side-by-side
+combined_plot_pollen <- 
+  waffle_plot_pollen_method + 
+  waffle_plot_pollen_source + 
+  process_column
+
+# 3. Add title & subtitle
+combined_plot_pollen <- combined_plot_pollen + 
+  plot_annotation(
+    title = title_combined_plot_pollen,
+    subtitle = st_combined_plot_pollen,
+    theme = theme(
+      plot.title = element_marquee(
+        size = 30,
+        width = 1,
+        hjust = 0
+      ),
+      plot.subtitle = element_marquee(
+        size = 15,
+        hjust = 0,
+        width = 1
+      )
+    )
+  )
+
+combined_plot_pollen
+
+
+
+
+
+
+
+
+
 
 
 
